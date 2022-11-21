37fecf3f0028aa4cdb778b2918b419bdbd064e38
psjoo7 <jmin9480@naver.com>
2022-11-21 15:59:56 +0900

    파이어베이스, 레이아웃 리뉴얼

--- app/src/main/java/com/example/googlemaptest/SignUpActivity.java
+++ app/src/main/java/com/example/googlemaptest/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.googlemaptest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +16,24 @@ import android.widget.Toast;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.SignInMethodQueryResult;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class SignUpActivity extends AppCompatActivity {
-    private EditText ID, Pwd, CheckPwd, Name, Add;
+
+    private FirebaseAuth mFirebaseAuth; // 파이어베이스 인증
+    private DatabaseReference mDatabaseRef; // 실시간 데이터베이스(서버에 연결)
+
+    private EditText ID, Pwd, Name, PhoneNum;
     private Button checkBtn, signUpBtn, backBtn;
     private AlertDialog dialog;
     private boolean validate = false;
@@ -32,12 +45,15 @@ public class SignUpActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-        ID = findViewById(R.id.userID);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("UserInfo");
+
+        ID = findViewById(R.id.userEmail);
         Pwd = findViewById(R.id.userPwd);
-        CheckPwd = findViewById(R.id.userCPwd);
         Name = findViewById(R.id.userName);
-        Add = findViewById(R.id.userAdd);
+        PhoneNum = findViewById(R.id.userPhoneNumber);
         checkBtn = findViewById(R.id.CheckButton);
+
         signUpBtn = findViewById(R.id.signupButton);
         backBtn = findViewById(R.id.BackButton);
         //로그인 화면으로 돌아가기
@@ -46,13 +62,16 @@ public class SignUpActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
-        //아이디 체크
+
+//        아이디 체크
         checkBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String UserID = ID.getText().toString();
+                FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
                 if (validate) {
                     return; //검증 완료
                 }
@@ -62,32 +81,22 @@ public class SignUpActivity extends AppCompatActivity {
                     dialog.show();
                     return;
                 }
-                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                //check email already exist or not.
+                mFirebaseAuth.fetchSignInMethodsForEmail(UserID).addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
                     @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-                            AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
-                            if (success) {
-                                dialog = builder.setMessage("사용할 수 있는 아이디입니다.").setPositiveButton("확인", null).create();
-                                dialog.show();
-                                ID.setEnabled(false); //아이디값 고정
-                                validate = true; //검증 완료
-                                checkBtn.setBackgroundColor(Color.GRAY);
-                            }
-                            else {
-                                dialog = builder.setMessage("이미 존재하는 아이디입니다.").setNegativeButton("확인", null).create();
-                                dialog.show();
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                    public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
+                        validate = task.getResult().getSignInMethods().isEmpty();
+                        if(validate){
+                            Toast.makeText(SignUpActivity.this, "사용할 수 있는 이메일입니다.", Toast.LENGTH_SHORT).show();
+                            validate = true;//검증 완료
+                            ID.setEnabled(false);//아이디값 고정
+                            checkBtn.setBackgroundColor(Color.GRAY);
+                        }
+                        else{
+                            Toast.makeText(SignUpActivity.this, "이미 존재하는 이메일입니다.", Toast.LENGTH_SHORT).show();
                         }
                     }
-                };
-                ValidateRequest validateRequest = new ValidateRequest(UserID, responseListener);
-                RequestQueue queue = Volley.newRequestQueue(SignUpActivity.this);
-                queue.add(validateRequest);
+                });
             }
         });
         // 회원가입 버튼 클릭시
@@ -97,10 +106,9 @@ public class SignUpActivity extends AppCompatActivity {
                 final String UserID = ID.getText().toString();
                 final String UserPwd = Pwd.getText().toString();
                 final String UserName = Name.getText().toString();
-                final String checkPwd = CheckPwd.getText().toString();
-                final String UserAdd = Add.getText().toString();
+                final String UserPhone = PhoneNum.getText().toString();
                 //한 칸이라도 입력 안했을 경우
-                if (UserID.equals("") || UserPwd.equals("") || UserName.equals("") || checkPwd.equals("") || UserAdd.equals("")) {
+                if (UserID.equals("") || UserPwd.equals("") || UserName.equals("") || UserPhone.equals("")) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
                     dialog = builder.setMessage("모두 입력해주세요.").setNegativeButton("확인", null).create();
                     dialog.show();
@@ -112,44 +120,27 @@ public class SignUpActivity extends AppCompatActivity {
                     dialog.show();
                     return;
                 }
-                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                mFirebaseAuth.createUserWithEmailAndPassword(UserID, UserPwd).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onResponse(String response) {
-
-                        try {
-                            JSONObject jsonObject = new JSONObject( response );
-                            boolean success = jsonObject.getBoolean( "success" );
-                            //회원가입 성공시
-                            if(UserPwd.equals(checkPwd)) {
-                                if (success) {
-                                    Toast.makeText(getApplicationContext(), String.format("%s님 가입을 환영합니다.", UserName), Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
-                                    startActivity(intent);
-                                    //회원가입 실패시
-                                } else {
-                                    Toast.makeText(getApplicationContext(), "회원가입에 실패하였습니다.", Toast.LENGTH_SHORT).show();
-                                    return;
-                                }
-                            } else {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
-                                dialog = builder.setMessage("비밀번호가 동일하지 않습니다.").setNegativeButton("확인", null).create();
-                                dialog.show();
-                                return;
-                            }
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                            UserAccount account = new UserAccount();
+                            account.setIdToken(firebaseUser.getUid());
+                            account.setEmailId(firebaseUser.getEmail());
+                            account.setPassword(UserPwd);
+                            account.setName(UserName);
+                            account.setPhoneNum(UserPhone);
+                            mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
+                            Toast.makeText(SignUpActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "회원가입 실패", Toast.LENGTH_SHORT).show();
                         }
-
                     }
-                };
-                //서버로 Volley를 이용해서 요청
-                JoinRequest registerRequest = new JoinRequest( UserID, UserPwd, UserName, UserAdd, responseListener);
-                RequestQueue queue;
-                queue = Volley.newRequestQueue( SignUpActivity.this );
-                queue.add( registerRequest );
-
-
+                });
             }
         });
     }
