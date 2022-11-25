01f4cf6e404f00a6f9c77f5f88e29e8c3813df01
psjoo7 <jmin9480@naver.com>
2022-11-25 19:38:09 +0900

    11월 25일 수정
    1. MainActivity
    - 퍼미션에 대한 함수들 정리
    - 현재 위치와 각 산들간의 거리 측정하는 함수 작성 완료
    - 해야될 일 : 현재 위치를 매번 받아오고, 산과의 거리를 그때마다 계산을 하는건
    매우 비효율적, 따라서 현재 위치를 특정 버튼을 눌렀을때만 할 수 있게 정리.
    2. startMountActivity
    - 현재 위치를 찾는 함수들 작성 완료
    - 해야될 일 : 끝이 났을때, 혹은 도달율 100퍼에 도달했을 때

---
+++ app/src/main/java/com/example/googlemaptest/ResultActivity.java
@@ -0,0 +1,28 @@
+package com.example.googlemaptest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class ResultActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+}
