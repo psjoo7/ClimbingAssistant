92869df4fab29faf6d750a2bd627223324236761
SunJin Moon <111959522+anjdfk111@users.noreply.github.com>
2022-11-27 13:16:52 +0900

    xml 세부 조정

--- app/src/main/java/com/example/googlemaptest/startMountActivity.java
+++ app/src/main/java/com/example/googlemaptest/startMountActivity.java
@@ -2,15 +2,28 @@ package com.example.googlemaptest;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.os.SystemClock;
 import android.util.Log;
 import android.widget.Chronometer;
 import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -25,18 +38,40 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class startMountActivity extends AppCompatActivity implements OnMapReadyCallback {
     private String MountName;
     private DatabaseReference mData, mRef;
     private String path;
     private GoogleMap mMap;
     private TextView mname;
+    private String userpath;
+    private Location location;
+    private LocationRequest locationRequest;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private TextView ArrivalRate;
+    public String elevation = "";
+    private static final int UPDATE_INTERVAL_MS = 1000;  // 1초
+    private static final int FASTEST_UPDATE_INTERVAL_MS = 500; // 0.5초
+
+    public boolean isend = false;
     private Chronometer stopWatch;
+    LinkedList<Location> location_info = new LinkedList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_mount);
-
         Intent getMainIntent = getIntent();
         MountName = getMainIntent.getStringExtra("MountName");
 
@@ -59,6 +94,18 @@ public class startMountActivity extends AppCompatActivity implements OnMapReadyC
         String time1 = timeHandler(currentTime, stopWatch);
         stopWatch.setBase(SystemClock.elapsedRealtime());
         stopWatch.start();
+        locationRequest = new LocationRequest()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL_MS)
+                .setFastestInterval(FASTEST_UPDATE_INTERVAL_MS);
+
+        LocationSettingsRequest.Builder builder =
+                new LocationSettingsRequest.Builder();
+
+        builder.addLocationRequest(locationRequest);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        ArrivalRate = findViewById(R.id.arrival_rate);
+
     }
 
     public String timeHandler(long time, Chronometer stopWatch)
@@ -105,6 +152,7 @@ public class startMountActivity extends AppCompatActivity implements OnMapReadyC
                 path = mountInfo.path;
                 Log.d("startpath", "getIntent1 "+ path);
                 drawLine(mMap, path);
+
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
@@ -112,6 +160,105 @@ public class startMountActivity extends AppCompatActivity implements OnMapReadyC
         });
     }
 
+    LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+
+            List<Location> locationList = locationResult.getLocations();
+            Log.d("cccc", "cccc3");
+
+            if (locationList.size() > 0) {
+
+                location = locationList.get(locationList.size() - 1);
+                if(location != null)
+                {
+                    userpath+=convertLocToString(location);
+
+                }
+                Log.d("userPath","userPath : " + userpath);
+
+                location_info.add(location);
+                //location = locationList.get(0);
+                LatLng currentPosition
+                        = new LatLng(location.getLatitude(), location.getLongitude());
+                Log.d("current Location"," : " + currentPosition.latitude + currentPosition.longitude );
+
+                new Thread(() -> {
+                    Looper.prepare();
+                    String ele = getElevation(location.getLatitude(), location.getLongitude());
+                    Handler handler = new Handler();
+                    Bundle bundle = new Bundle();
+                    Message message = new Message();
+                    bundle.putString("elevation", ele);
+                    message.setData(bundle);
+                    handler.sendMessage(message);
+                    Log.d("elevation", " : "+ ele);
+                    elevation = ele;
+
+                }).start();
+                if(elevation.length()>10)
+                {
+                    ArrivalRate.setText(elevation.substring(0,5));
+
+                }
+                else
+                {
+                    ArrivalRate.setText("0");
+                }
+                String Lat1 = String.valueOf(location.getLatitude());
+                String Long1 = String.valueOf(location.getLongitude());
+
+
+                String.valueOf(location.getLatitude());
+
+                PolylineOptions polylineOptions = new PolylineOptions();
+
+            }
+        }
+    };
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("TAG", "onStart");
+        Log.d("cccc", "cccc5");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if (mMap!=null)
+            mMap.setMyLocationEnabled(true);
+        Log.d("Start","위치시작");
+        mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+//        if (checkPermission()) {
+//
+//            Log.d(TAG, "onStart : call mFusedLocationClient.requestLocationUpdates");
+//            mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+//
+//            if (mMap!=null)
+//                mMap.setMyLocationEnabled(true);
+//        }
+    }
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        //다음 화면으로 전환
+        //다음 화면에 넘길 것들 : 경로(userPath) , 고도(elevation) , 시간
+    }
+
+    private String convertLocToString(Location location)
+    {
+        String coordinate = String.valueOf(location.getLatitude())+","+ String.valueOf(location.getLongitude())+"/";
+        return coordinate;
+    }
 
     public void drawLine(GoogleMap Map , String path){
         PolylineOptions polylineOptions = new PolylineOptions();
@@ -130,4 +277,30 @@ public class startMountActivity extends AppCompatActivity implements OnMapReadyC
         }
         catch (Exception e){}
     }
+
+    public String getElevation(double latitude, double longitude) {
+        try {
+            String key = "AIzaSyCGQ8fB1BVIr4mq-lHuxjaQy4EvFomeXaQ";
+            String url = "https://maps.googleapis.com/maps/api/elevation/json";
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .build();
+            Request request = new Request.Builder()
+                    .url("https://maps.googleapis.com/maps/api/elevation/json?locations=" + latitude + "%2C" + longitude + "&key=" + key)
+                    .method("GET", null)
+                    .build();
+
+            Response response = client.newCall(request).execute();
+            String msg = response.body().string();
+//            System.out.println(msg);
+            JSONObject object = new JSONObject(msg);
+            String results = object.getString("results");
+            JSONArray objectArr = new JSONArray(results);
+            JSONObject result = new JSONObject(objectArr.getString(0));
+            String ele = result.getString("elevation");
+            return ele;
+        } catch (Exception e) {
+            return (e.toString());
+        }
+    }
+
 }
