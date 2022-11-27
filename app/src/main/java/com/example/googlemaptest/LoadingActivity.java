6fe3f67f9e83b6bba23479932f102e79164f338f
kookso <117020362+kookso@users.noreply.github.com>
2022-11-27 18:53:17 +0900

    아이디 찾기 11/27

--- app/src/main/java/com/example/googlemaptest/LoadingActivity.java
+++ app/src/main/java/com/example/googlemaptest/LoadingActivity.java
@@ -3,6 +3,8 @@ package com.example.googlemaptest;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.content.Intent;
+
 
 public class LoadingActivity extends Activity {
 
@@ -10,12 +12,23 @@ public class LoadingActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        startLoading();
 
+        /* 이거 애니메이션 버전인데 이거 오늘까지 수정함 일단 합치기 위해 올림
         Intent intent = new Intent(LoadingActivity.this,LoginActivity.class);
         startActivity(intent);
         //다음 화면에 실행할 애니메이션, 현재 화면에 실행할 애니메이션
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-        finish();
+        finish(); */
 
+    }
+    private void startLoading() {
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                finish();
+            }
+        },3000);
     }
 }
