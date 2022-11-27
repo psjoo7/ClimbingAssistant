6f862b82d2b622ebaf42bed6ef2597e94ea864ad
ChamsolPark <66250400+ChamsolPark@users.noreply.github.com>
2022-11-28 00:39:23 +0900

    날씨추가

--- app/src/main/java/com/example/googlemaptest/MainActivity.java
+++ app/src/main/java/com/example/googlemaptest/MainActivity.java
@@ -1,16 +1,5 @@
 package com.example.googlemaptest;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -28,6 +17,14 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -53,14 +50,16 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONException;
+
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
-
-
 public class MainActivity extends AppCompatActivity
         implements OnMapReadyCallback,
         ActivityCompat.OnRequestPermissionsResultCallback, GoogleMap.OnMarkerClickListener {
@@ -71,11 +70,11 @@ public class MainActivity extends AppCompatActivity
     private static final String TAG = "googlemap_example";
     private static final int GPS_ENABLE_REQUEST_CODE = 2001;
     private static final int UPDATE_INTERVAL_MS = 1000;  // 1초
-    private static final int FASTEST_UPDATE_INTERVAL_MS = 500; // 0.5초
-
+    private static final int FASTEST_UPDATE_INTERVAL_MS = 500; // 0.5초private GpsTracker gpsTracker;
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
     Location mCurrentLocatiion;
     LatLng currentPosition;
-
     List<MountElement> mount = new ArrayList<>();
     private FusedLocationProviderClient mFusedLocationClient;
     private LocationRequest locationRequest;
@@ -94,11 +93,161 @@ public class MainActivity extends AppCompatActivity
     private View mLayout;  // Snackbar 사용하기 위해서는 View가 필요합니다.
     // (참고로 Toast에서는 Context가 필요했습니다.)
 
+    public String getCurrentAddress( double latitude, double longitude) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses;
+        try {
+            addresses = geocoder.getFromLocation(
+                    latitude,
+                    longitude,
+                    100);
+        } catch (IOException ioException) {
+            //네트워크 문제
+            Toast.makeText(this, "지오코더 서비스 사용불가", Toast.LENGTH_LONG).show();
+            showDialogForLocationServiceSetting();
+            return "지오코더 서비스 사용불가";
+        } catch (IllegalArgumentException illegalArgumentException) {
+            Toast.makeText(this, "잘못된 GPS 좌표", Toast.LENGTH_LONG).show();
+            showDialogForLocationServiceSetting();
+            return "잘못된 GPS 좌표";
+        }
+        if (addresses == null || addresses.size() == 0) {
+            Toast.makeText(this, "주소 미발견", Toast.LENGTH_LONG).show();
+            showDialogForLocationServiceSetting();
+            return "주소 미발견";
+        }
+        Address address = addresses.get(0);
+        return address.getAddressLine(0).toString()+"\n";
+    }
+    //여기부터는 GPS 활성화를 위한 메소드들
+    private void showDialogForLocationServiceSetting() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("위치 서비스 비활성화");
+        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
+                + "위치 설정을 수정하실래요?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                Intent callGPSSettingIntent
+                        = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+        builder.create().show();
+    }
+    void checkRunTimePermission() {
+
+        //런타임 퍼미션 처리
+        // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
+        int hasFineLocationPermission = ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+
+        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
+
+        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
+
+            // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, REQUIRED_PERMISSIONS[0])) {
+
+                // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
+                Toast.makeText(MainActivity.this, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+                // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE);
+
+
+            } else {
+                // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
+                // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE);
+            }
+
+        }
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case GPS_ENABLE_REQUEST_CODE:
+                //사용자가 GPS 활성 시켰는지 검사
+                if (checkLocationServicesStatus()) {
+                    if (checkLocationServicesStatus()) {
+                        Log.d("@@@", "onActivityResult : GPS 활성화 되있음");
+                        checkRunTimePermission();
+                        return;
+                    }
+                }
+                break;
+        }
+    }
+    public boolean checkLocationServicesStatus() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    private String x = "", y = "", address = "";
+
+    private String getTime() {
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat dateFormat1 = new SimpleDateFormat("yyyyMMdd");
+        String date1 = dateFormat1.format(date);
+
+        return date1;
+    }
+
+    private String getTime1() {
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat dateFormat2 = new SimpleDateFormat("HHmm");
+        String date2 = dateFormat2.format(date);
+        return date2;
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+        super.onCreate(savedInstanceState);
+        GpsTracker gpsTracker = new GpsTracker(this);
+        double latitude = gpsTracker.getLatitude();
+        double longitude = gpsTracker.getLongitude();
+
+        String address = getCurrentAddress(latitude, longitude);
+        String[] local = address.split(" ");
+        String localName = local[2];
+
+        String weather = "";
+        WeatherData wd = new WeatherData();
+        try {
+            // date와 time에 값을 넣어야함
+            // ex) date = "20210722", time = "0500"
+            weather = wd.lookUpWeather(getTime(), getTime1(), x, y);
+        } catch (IOException e) {
+            Log.i("THREE_ERROR1", e.getMessage());
+        } catch (JSONException e) {
+            Log.i("THREE_ERROR2", e.getMessage());
+        }
+        Log.i("현재날씨",weather);
+
+
+
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
                 WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         setContentView(R.layout.activity_main);
---
+++ app/src/main/java/com/example/googlemaptest/WeatherData.java
@@ -0,0 +1,170 @@
+package com.example.googlemaptest;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.WorkSource;
+import android.util.Log;
+import org.json.JSONException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+import java.text.SimpleDateFormat;
+
+public class WeatherData{
+
+    private String weather = "", tmperature = "";
+
+    public String lookUpWeather(String baseDate, String time, String nx, String ny) throws IOException, JSONException {
+
+//      String baseDate = date;//"20210531";//date	//조회하고싶은 날짜
+        String baseTime = timeChange(time); //"0500";//timeChange(time);	//조회하고싶은 시간
+        String type = "json";    //조회하고싶은 시간
+
+        String apiUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst";
+//         홈페이지에서 받은 키
+        String serviceKey = "8bBHdSOqe1GzRlJ%2Be6VPGq%2Bp2Q6hcS21Y8dcZwfd6dKrhbVApvbfzgpubXrCI81GqupGRtbuUdK6%2BOy6vt8MiA%3D%3D";
+
+        StringBuilder urlBuilder = new StringBuilder(apiUrl);
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + serviceKey);
+        urlBuilder.append("&" + URLEncoder.encode("nx", "UTF-8") + "=" + URLEncoder.encode(nx, "UTF-8")); //경도
+        urlBuilder.append("&" + URLEncoder.encode("ny", "UTF-8") + "=" + URLEncoder.encode(ny, "UTF-8")); //위도
+        urlBuilder.append("&" + URLEncoder.encode("base_date", "UTF-8") + "=" + URLEncoder.encode(baseDate, "UTF-8")); /* 조회하고싶은 날짜*/
+        urlBuilder.append("&" + URLEncoder.encode("base_time", "UTF-8") + "=" + URLEncoder.encode(baseTime, "UTF-8")); /* 조회하고싶은 시간 AM 02시부터 3시간 단위 */
+        urlBuilder.append("&" + URLEncoder.encode("dataType", "UTF-8") + "=" + URLEncoder.encode(type, "UTF-8"));    /* 타입 */
+
+        /*
+         * GET방식으로 전송해서 파라미터 받아오기
+         */
+        URL url = new URL(urlBuilder.toString());
+        //어떻게 넘어가는지 확인하고 싶으면 아래 출력분 주석 해제
+        //System.out.println(url);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+
+        BufferedReader rd;
+        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+        String result = sb.toString();
+
+
+        // response 키를 가지고 데이터를 파싱
+        JSONObject jsonObj_1 = new JSONObject(result);
+        String response = jsonObj_1.getString("response");
+
+        // response 로 부터 body 찾기
+        JSONObject jsonObj_2 = new JSONObject(response);
+        String body = jsonObj_2.getString("body");
+
+        // body 로 부터 items 찾기
+        JSONObject jsonObj_3 = new JSONObject(body);
+        String items = jsonObj_3.getString("items");
+        Log.i("ITEMS", items);
+
+        // items로 부터 itemlist 를 받기
+        JSONObject jsonObj_4 = new JSONObject(items);
+        JSONArray jsonArray = jsonObj_4.getJSONArray("item");
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            jsonObj_4 = jsonArray.getJSONObject(i);
+            String fcstValue = jsonObj_4.getString("fcstValue");
+            String category = jsonObj_4.getString("category");
+
+            if (category.equals("SKY")) {
+
+                if (fcstValue.equals("1")) {
+                    weather = "맑음";
+                } else if (fcstValue.equals("2")) {
+                    weather = "비";
+                } else if (fcstValue.equals("3")) {
+                    weather = "구름";
+                } else if (fcstValue.equals("4")) {
+                    weather = "흐림";
+                }
+            }
+
+            if (category.equals("T3H") || category.equals("T1H")) {
+                tmperature = fcstValue;
+            }
+
+            Log.i("날씨", fcstValue);
+            Log.i("카테고리", category);
+
+            Log.i("", weather + tmperature);
+        }
+
+        return weather + tmperature;
+    }
+
+    public String timeChange(String time)
+    {
+        // 현재 시간에 따라 데이터 시간 설정(3시간 마다 업데이트) //
+        /**
+         시간은 3시간 단위로 조회해야 한다. 안그러면 정보가 없다고 뜬다.
+         0200, 0500, 0800 ~ 2300까지
+         그래서 시간을 입력했을때 switch문으로 조회 가능한 시간대로 변경해주었다.
+         **/
+        switch(time) {
+
+            case "0200":
+            case "0300":
+            case "0400":
+                time = "0200";
+                break;
+            case "0500":
+            case "0600":
+            case "0700":
+                time = "0500";
+                break;
+            case "0800":
+            case "0900":
+            case "1000":
+                time = "0800";
+                break;
+            case "1100":
+            case "1200":
+            case "1300":
+                time = "1100";
+                break;
+            case "1400":
+            case "1500":
+            case "1600":
+                time = "1400";
+                break;
+            case "1700":
+            case "1800":
+            case "1900":
+                time = "1700";
+                break;
+            case "2000":
+            case "2100":
+            case "2200":
+                time = "2000";
+                break;
+            default:
+                time = "2300";
+
+        }
+        return time;
+    }
+
+}
