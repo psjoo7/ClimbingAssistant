2214948c9e1872fd1d595ed73669134f61014364
kookso <117020362+kookso@users.noreply.github.com>
2022-11-27 15:46:41 +0900

    아이디 찾기 11/27

---
+++ app/src/main/java/com/example/googlemaptest/FindIdActivity.java
@@ -0,0 +1,13 @@
+package com.example.googlemaptest;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FindIdActivity extends AppCompatActivity {
+    @override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find);
+    }
+}
